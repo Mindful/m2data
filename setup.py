a34a981f25637e32f0c087bed9674d5579b1e8cb
Joshua Tanner <mindful.jt@gmail.com>
2021-04-02 18:46:51 +0900

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="m2data",
-    version="0.0.12",
+    version="0.0.13",
     author="Joshua Tanner",
     author_email="mindful.jt@gmail.com",
     description="A package for reading .m2 files commonly used in GEC tasks",
